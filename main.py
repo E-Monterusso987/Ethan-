b18d6d3ee91141fc1835cc252cc8d3b8c846a0f0
E-Monterusso987 <152335113+E-Monterusso987@users.noreply.github.com>
2023-11-29 14:13:18 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+count = 0
+
+
+def guessingFood():
+  guess = input('Guess my favorite food: ')
+  global count
+  count = count + 1
+  if (guess != 'Rice'):
+    guessingFood()
+
+
+def guessingDrink():
+  guess = input('Guess my favorite drink: ')
+  global count
+  count = count + 1
+  if (guess != 'Water'):
+    guessingDrink()
+
+
+print('Welcome to the best friend test.')
+guessingFood()
+print('you got it!')
+guessingDrink()
+print('you got it!')
+if count < 5:
+  print('You know me well.')
+elif count < 10:
+  print('We should probably talk more.')
+else:
+  print('You are a disgrace!')
